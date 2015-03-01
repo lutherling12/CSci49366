8c56ac1c0cd30a1f5512e800e39842f9f76f649d
Luther <lutherling12@gmail.com>
2015-02-28 21:53:49 -0500

    Made makefile for hw2

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -3,15 +3,42 @@
 
 #include <fcntl.h>
 
+#include <string.h>
+
+void removeSpaces (char * str);
+
 int main (int argc, char * argv [])
 {
 	char title[200];
 	char author [200];
 
-	int fd = open ("books.txt", O_RDWR);
-	printf ("%s %i", "Books fd is: ", fd);
+	char sample[] = "Remove my spaces";	
 
-	return 0;
+	printf ("%i\n", (int)strlen(sample));
+	removeSpaces(sample);
+	printf ("\"%s\"\n", sample);
 
-sdsdds
+	//int fd = open ("books.txt", O_RDWR);
+
+	/*
+	if (fd != -1) {
+		printf ("%s %i\n", "books.txt fd is: ", fd);
+		return 0;
+	}
+	else {
+		printf ("%s\n", "books.txt file not found.");
+		return 1;
+	}
+	*/
+}
+
+void removeSpaces (char str[]) {
+	int length = strlen (str);
+
+	for (int i = 0; i < length-1; i++) {
+		if (str[i] == ' ') {
+			printf("%c", str[i]);
+			str[i] = '-';
+		}
+	}
 }
