ee4a2e7fa18fb20a277716bca1b7c9f0ecdc121c
Luther <lutherling12@gmail.com>
2015-02-28 20:34:55 -0500

    Getting started on hw2

---
+++ hw2/hw2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <fcntl.h>
+
+int main (int argc, char * argv [])
+{
+	char title[200];
+	char author [200];
+
+	int fd = open ("books.txt", O_RDWR);
+	printf ("%s %i", "Books fd is: ", fd);
+
+	return 0;
+
+}
