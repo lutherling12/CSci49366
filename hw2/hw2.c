1a1e1302da32af5b20f768f817731c0b12e132ec
Luther <lutherling12@gmail.com>
2015-02-28 22:59:33 -0500

    takes arguments, replaces spaces with underscores

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -5,40 +5,61 @@
 
 #include <string.h>
 
-void removeSpaces (char * str);
+#define T_BUFFER_SIZE 256
+#define A_BUFFER_SIZE 256
+
+void replaceSpaces (char str[], char replacementChar = '_');
 
 int main (int argc, char * argv [])
 {
-	char title[200];
-	char author [200];
+	char title[A_BUFFER_SIZE];
+	char author [T_BUFFER_SIZE];
 
-	char sample[] = "Remove my spaces";	
+	int fd = open ("books.txt", O_RDWR);
+	off_t offsetNow;
 
-	printf ("%i\n", (int)strlen(sample));
-	removeSpaces(sample);
-	printf ("\"%s\"\n", sample);
+	int rowNum = 0;
 
-	//int fd = open ("books.txt", O_RDWR);
-
-	/*
-	if (fd != -1) {
-		printf ("%s %i\n", "books.txt fd is: ", fd);
-		return 0;
-	}
-	else {
-		printf ("%s\n", "books.txt file not found.");
+	if (argc != 4) {
+		printf ("%s\n", 
+			"command line arguments are \"<lineNum> <title> <author>\".");
 		return 1;
 	}
-	*/
+
+	rowNum = atoi (argv[1]);
+
+	if (rowNum < 1) {
+		printf ("%s\n", "first argument invalid row number.");
+		return 1;
+	}
+
+	if (fd == -1) {
+		printf ("%s\n", "books.txt file not found in pwd.");
+		return 1;
+	}
+
+	else {
+		printf ("%s %i\n", "books.txt fd is: ", fd);
+
+		strncpy (title, argv[2], T_BUFFER_SIZE);
+		strncpy (author, argv[3], T_BUFFER_SIZE);
+
+		replaceSpaces (title);
+		replaceSpaces (author);
+
+		printf ("%s %s\n", title, author);
+
+		return 0;
+	}
 }
 
-void removeSpaces (char str[]) {
+void replaceSpaces (char str[], char replacementChar) {
 	int length = strlen (str);
 
 	for (int i = 0; i < length-1; i++) {
 		if (str[i] == ' ') {
 			printf("%c", str[i]);
-			str[i] = '-';
+			str[i] = replacementChar;
 		}
 	}
 }
