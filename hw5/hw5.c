ace4c3ed0e47de41383f147781af4ba8e595741a
Luther <lutherling12@gmail.com>
2015-04-21 17:32:59 -0400

    formatting

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -4,16 +4,42 @@
 #include <limits.h>
 #include <unistd.h>
 
+void printHeader ();
+
 int main (int argc, char ** argv) {
   struct passwd * temp;
   
   for (int i=0; i <= INT_MAX; i++) {
+    if (i % 20 == 0) {
+      void printHeader ();
+    }
+
     temp = getpwuid (i);
+
     if (temp != NULL) {
-      printf ("%s\n", temp->pw_name);
+      printf ("%*i |", +10, i);
+      printf ("%*s\n", -20, temp->pw_name);
+    }
+    else {
+      printf ("%*i\n", +10, i);
+      printf ("%*s\n", -20, "Unassigned");
     }
     sleep (1); 
   }
 
   return 0;
+}
+
+void printHeader () {
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }
+
+  printf ("%*s", +10, "ID Number");
+  printf ("%*s", -20, "Username");
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }  
 }
