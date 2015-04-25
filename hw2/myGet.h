d822bd48248fff404def7fbf17ab8b1803ecb31f
Luther <lutherling12@gmail.com>
2015-04-24 20:11:31 -0400

    Hw2 comments.

--- hw2/myGet.h
+++ hw2/myGet.h
@@ -1,6 +1,22 @@
 /******************************************************************************
+Title:    Unix Tools - Assignment 2 - myGet
+Author:   Luther Ling
+
+Build:    make myGet.o
+Usage:    with hw2.c
+
+Description:
+
 These functions read from an input file and leaves the offset where it is when
 it is done; they alter and displace the offset when called. 
+
+getChar takes the file descriptor of an open file. Returns the next character 
+from the offset.
+
+getALine takes the file descriptor of an open file, a container in which to store that in,
+******************************************************************************/
+
+/******************************************************************************
 
 Or, they perform "gets" into the file.
 ******************************************************************************/
--- hw2/myWrites.h
+++ hw2/myWrites.h
@@ -1,8 +1,20 @@
 /******************************************************************************
-Performs writes to a file.
-Offset for open file changes
-******************************************************************************/
+Title:    Unix Tools - Assignment 2 - myWrites
+Author:   Luther Ling
 
+Build:    make myWrites.o
+Usage:    with hw2.c
+
+Description:
+
+writeStr expects the file descriptor of an open file, and writes string str
+into the open file. Returns nothing.
+
+shiftWrite expects the file descriptor of an open file and shifts the contents 
+of the file by shiftBytes number of bytes starting from the offset of the 
+open file. It does so via tail recursion. Returns 1 if not EOF for the next 
+iteration of recursion, else returns 0.
+******************************************************************************/
 #ifndef MY_WRITES
 #define MY_WRITES
 
