64666ffba5672e9e2cb3311116ce7ac6f180e731
Luther <lutherling12@gmail.com>
2015-04-27 12:10:47 -0400

    Hw 6 directory made.

---
+++ hw5/hw5.1.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+#include <pwd.h>
+#include <grp.h>
+#include <sys/types.h>
+
+void printHeader ();
+
+int main (int argc, char ** argv) {
+  
+  struct passwd * temp_pw;
+  gid_t temp_gid;
+  struct group * temp_gr;
+
+  for (int i=0; i <= INT_MAX; i++) {
+    if ((i % 20) == 0) {
+      printHeader ();
+    }
+
+    temp_pw = getpwuid (i);
+
+    if (temp_pw != NULL) {
+      printf ("%*i |", +10, i);
+      printf ("%*s", -20, temp_pw->pw_name);
+      temp_gid = temp_pw->pw_gid;
+      temp_gr = getgrgid (temp_gid);
+      printf ("%*i |", +10, temp_gid);
+      printf ("%*s\n", -20, temp_gr->gr_name);
+    }
+    else {
+      printf ("%*i |", +10, i);
+      printf ("%*s\n", -20, "");
+    }
+    sleep (1); 
+  }
+
+  return 0;
+}
+
+void printHeader () {
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }
+
+  printf ("%*s", +10, "  ID Number|");
+  printf ("%*s", -20, "Username");
+  printf ("%*s", +10, " GID Number|");
+  printf ("%*s\n", +10, "Group Number");
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }  
+}
---
+++ hw5/hw5.2.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <grp.h>
+
+void printHeader ();
+
+int main (int argc, char ** argv) {
+  
+  struct group * temp_gr;
+
+  for (int i=0; i <= INT_MAX; i++) {
+    if ((i % 20) == 0) {
+      printHeader ();
+    }
+
+    temp_gr = getgrgid ((gid_t)i);
+      
+    if (temp_gr != NULL) {
+      printf ("%*i |", +10, i);
+      printf ("%*s\n", -20, temp_gr->gr_name);
+    }
+    else {
+      printf ("%*i |", +10, i);
+      printf ("%*s\n", -20, "");
+    }
+    sleep (1);
+  }
+
+  return 0;
+}
+
+void printHeader () {
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }
+
+  printf ("%*s", +10, "   Group ID|");
+  printf ("%*s\n", -20, "Group Name");
+
+  for (int i = 1; i <= 80; i++) {
+    printf ("*");
+  }  
+}
