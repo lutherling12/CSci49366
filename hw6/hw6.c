f184a04b16aceec4098dea6c95aa345f2d8a1a3a
Luther <lutherling12@gmail.com>
2015-05-05 19:00:58 -0400

    Included other statuses.

--- hw6/hw6.c
+++ hw6/hw6.c
@@ -1,6 +1,12 @@
 /*Wait3() is considered "obsolete" and waitpid/waitid are suggested 
 alternatives, but  the assignment calls for usage of wait3() due to
 collection of resource usage (rusage) of the child(?) process.*/
+
+//Removes "implicit declaration" warning for kill().
+#define _POSIX_SOURCE
+//Removes "implicit declaration" warning for vfork() and wait3().
+#define _BSD_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -113,157 +119,145 @@ void findMacro (const int termStatus) {
 void printSig (int signum) {
   switch (signum) {
     case SIGABRT:
-      printf("%*s (%i)\n", -25, "SIGABRT: ", SIGABRT);
+      printf("%*s %i\n", 25, "SIGABRT: ", SIGABRT);
       break;
-    /*
-      case SIGALRM:
-        printf("%*s (%i)\n", -25, "SIGALRM: ", SIGALRM);
-        break;
-      case SIGBUS:
-        printf("%*s (%i)\n", -25, "SIGBUS: ", SIGBUS);
-        break;
-      case SIGCANCEL:
-        printf("%*s (%i)\n", -25, "SIGCANCEL: ", SIGCANCEL);
-        break;
-      case SIGCHLD:
-        printf("%*s (%i)\n", -25, "SIGCHLD: ", SIGCHLD);
-        break;
-    */
-      case SIGCONT:
-        printf("%*s (%i)\n", -25, "SIGCONT: ", SIGCONT);
-        break;
-    /*
-      case SIGEMT:
-        printf("%*s (%i)\n", -25, "SIGEMT: ", SIGEMT);
-        break;
-    */
+    case SIGALRM:
+      printf("%*s %i\n", 25, "SIGALRM: ", SIGALRM);
+      break;
+    case SIGBUS:
+      printf("%*s %i\n", 25, "SIGBUS: ", SIGBUS);
+      break;
+    case SIGCHLD:
+      printf("%*s %i\n", 25, "SIGCHLD: ", SIGCHLD);
+      break;
+    case SIGCONT:
+      printf("%*s %i\n", 25, "SIGCONT: ", SIGCONT);
+      break;
     case SIGFPE:
-      printf("%*s (%i)\n", -25, "SIGFPE: ", SIGFPE);
+      printf("%*s %i\n", 25, "SIGFPE: ", SIGFPE);
       break;
-    /*
-      case SIGFREEZE:
-        printf("%*s (%i)\n", -25, "SIGFREEZE: ", SIGFREEZE);
-        break;
-      case SIGHUP:
-        printf("%*s (%i)\n", -25, "SIGHUP: ", SIGHUP);
-        break;
-    */
+    case SIGHUP:
+      printf("%*s %i\n", 25, "SIGHUP: ", SIGHUP);
+      break;
     case SIGILL:
-      printf("%*s (%i)\n", -25, "SIGILL: ", SIGILL);
+      printf("%*s %i\n", 25, "SIGILL: ", SIGILL);
       break;
-    /*
-      case SIGINFO:
-        printf("%*s (%i)\n", -25, "SIGINFO: ", SIGINFO);
-        break;
-    */
     case SIGINT:
-      printf("%*s (%i)\n", -25, "SIGINT: ", SIGINT);
+      printf("%*s %i\n", 25, "SIGINT: ", SIGINT);
       break;
-    /*
-      case SIGIO:
-        printf("%*s (%i)\n", -25, "SIGIO: ", SIGIO);
-        break;
-      //SIGIOT is equal to SIGABRT on Ubuntu 14.04.
-      case SIGIOT:
-        printf("%*s (%i)\n", -25, "SIGIOT: ", SIGIOT);
-        break;
-      case SIGJVM1:
-        printf("%*s (%i)\n", -25, "SIGJVM1: ", SIGJVM1);
-        break;
-      case SIGJVM2:
-        printf("%*s (%i)\n", -25, "SIGJVM2: ", SIGJVM2);
-        break;
-      case SIGKILL:
-        printf("%*s (%i)\n", -25, "SIGKILL: ", SIGKILL);
-        break;
-      case SIGLOST:
-        printf("%*s (%i)\n", -25, "SIGLOST: ", SIGLOST);
-        break;
-      case SIGLWP:
-        printf("%*s (%i)\n", -25, "SIGLWP: ", SIGLWP);
-        break;
-      case SIGPIPE:
-        printf("%*s (%i)\n", -25, "SIGPIPE: ", SIGPIPE);
-        break;
-      case SIGPOLL:
-        printf("%*s (%i)\n", -25, "SIGPOLL: ", SIGPOLL);
-        break;
-      case SIGPROF:
-        printf("%*s (%i)\n", -25, "SIGPROF: ", SIGPROF);
-        break;
-      case SIGPWR:
-        printf("%*s (%i)\n", -25, "SIGPWR: ", SIGPWR);
-        break;
-      case SIGQUIT:
-        printf("%*s (%i)\n", -25, "SIGQUIT: ", SIGQUIT);
-        break;
-    */
+    case SIGIO:
+      printf("%*s %i\n", 25, "SIGIO: ", SIGIO);
+      break;
+    case SIGKILL:
+      printf("%*s %i\n", 25, "SIGKILL: ", SIGKILL);
+      break;
+    case SIGPIPE:
+      printf("%*s %i\n", 25, "SIGPIPE: ", SIGPIPE);
+      break;
+    case SIGPROF:
+      printf("%*s %i\n", 25, "SIGPROF: ", SIGPROF);
+      break;
+    case SIGPWR:
+      printf("%*s %i\n", 25, "SIGPWR: ", SIGPWR);
+      break;
+    case SIGQUIT:
+      printf("%*s %i\n", 25, "SIGQUIT: ", SIGQUIT);
+      break;
     case SIGSEGV:
-      printf("%*s (%i)\n", -25, "SIGSEGV: ", SIGSEGV);
+      printf("%*s %i\n", 25, "SIGSEGV: ", SIGSEGV);
       break;
-    /*
-      case SIGSTKFLT:
-        printf("%*s (%i)\n", -25, "SIGSTKFLT: ", SIGSTKFLT);
-        break;
-    */
-      case SIGSTOP:
-        printf("%*s (%i)\n", -25, "SIGSTOP: ", SIGSTOP);
-        break;
-    /*
-      case SIGSYS:
-        printf("%*s (%i)\n", -25, "SIGSYS: ", SIGSYS);
-        break;
-    */
+    case SIGSTKFLT:
+      printf("%*s %i\n", 25, "SIGSTKFLT: ", SIGSTKFLT);
+      break;
+    case SIGSTOP:
+      printf("%*s %i\n", 25, "SIGSTOP: ", SIGSTOP);
+      break;
+    case SIGSYS:
+      printf("%*s %i\n", 25, "SIGSYS: ", SIGSYS);
+      break;
     case SIGTERM:
-      printf("%*s (%i)\n", -25, "SIGTERM: ", SIGTERM);
+      printf("%*s %i\n", 25, "SIGTERM: ", SIGTERM);
       break;
-    /*
-      case SIGTHAW:
-        printf("%*s (%i)\n", -25, "SIGTHAW: ", SIGTHAW);
-        break;
-      case SIGTHR:
-        printf("%*s (%i)\n", -25, "SIGTHR: ", SIGTHR);
-        break;
-      case SIGTRAP:
-        printf("%*s (%i)\n", -25, "SIGTRAP: ", SIGTRAP);
-        break;
-      case SIGTSTP:
-        printf("%*s (%i)\n", -25, "SIGTSTP: ", SIGTSTP);
-        break;
-      case SIGTTIN:
-        printf("%*s (%i)\n", -25, "SIGTTIN: ", SIGTTIN);
-        break;
-      case SIGTTOU:
-        printf("%*s (%i)\n", -25, "SIGTTOU: ", SIGTTOU);
-        break;
-      case SIGURG:
-        printf("%*s (%i)\n", -25, "SIGURG: ", SIGURG);
-        break;
-      case SIGUSR1:
-        printf("%*s (%i)\n", -25, "SIGUSR1: ", SIGUSR1);
-        break;
-      case SIGUSR2:
-        printf("%*s (%i)\n", -25, "SIGUSR2: ", SIGUSR2);
-        break;
-      case SIGVTALRM:
-        printf("%*s (%i)\n", -25, "SIGVTALRM: ", SIGVTALRM);
-        break;
-      case SIGWAITING:
-        printf("%*s (%i)\n", -25, "SIGWAITING: ", SIGWAITING);
-        break;
-      case SIGWINCH:
-        printf("%*s (%i)\n", -25, "SIGWINCH: ", SIGWINCH);
-        break;
-      case SIGXCPU:
-        printf("%*s (%i)\n", -25, "SIGXCPU: ", SIGXCPU);
-        break;
-      case SIGXFSZ:
-        printf("%*s (%i)\n", -25, "SIGXFSZ: ", SIGXFSZ);
-        break;
-      case SIGXRES:
-        printf("%*s (%i)\n", -25, "SIGXRES: ", SIGXRES);
-        break;
-    */
+    case SIGTRAP:
+      printf("%*s %i\n", 25, "SIGTRAP: ", SIGTRAP);
+      break;
+    case SIGTSTP:
+      printf("%*s %i\n", 25, "SIGTSTP: ", SIGTSTP);
+      break;
+    case SIGTTIN:
+      printf("%*s %i\n", 25, "SIGTTIN: ", SIGTTIN);
+      break;
+    case SIGTTOU:
+      printf("%*s %i\n", 25, "SIGTTOU: ", SIGTTOU);
+      break;
+    case SIGURG:
+      printf("%*s %i\n", 25, "SIGURG: ", SIGURG);
+      break;
+    case SIGUSR1:
+      printf("%*s %i\n", 25, "SIGUSR1: ", SIGUSR1);
+      break;
+    case SIGUSR2:
+      printf("%*s %i\n", 25, "SIGUSR2: ", SIGUSR2);
+      break;
+    case SIGVTALRM:
+      printf("%*s %i\n", 25, "SIGVTALRM: ", SIGVTALRM);
+      break;
+    case SIGWINCH:
+      printf("%*s %i\n", 25, "SIGWINCH: ", SIGWINCH);
+      break;
+    case SIGXCPU:
+      printf("%*s %i\n", 25, "SIGXCPU: ", SIGXCPU);
+      break;
+    case SIGXFSZ:
+      printf("%*s %i\n", 25, "SIGXFSZ: ", SIGXFSZ);
+      break;
+  /*
+    //The following signals are undefined, or duplicated.
+    case SIGCANCEL:
+      printf("%*s %i\n", 25, "SIGCANCEL: ", SIGCANCEL);
+      break;
+    case SIGEMT:
+      printf("%*s %i\n", 25, "SIGEMT: ", SIGEMT);
+      break;
+    case SIGFREEZE:
+      printf("%*s %i\n", 25, "SIGFREEZE: ", SIGFREEZE);
+      break;
+    case SIGINFO:
+      printf("%*s %i\n", 25, "SIGINFO: ", SIGINFO);
+      break;
+    //SIGIOT is equal to SIGABRT on Ubuntu 14.04.
+    case SIGIOT:
+      printf("%*s %i\n", 25, "SIGIOT: ", SIGIOT);
+      break;
+    case SIGJVM1:
+      printf("%*s %i\n", 25, "SIGJVM1: ", SIGJVM1);
+      break;
+    case SIGJVM2:
+      printf("%*s %i\n", 25, "SIGJVM2: ", SIGJVM2);
+      break;
+    case SIGLOST:
+      printf("%*s %i\n", 25, "SIGLOST: ", SIGLOST);
+      break;
+    case SIGLWP:
+      printf("%*s %i\n", 25, "SIGLWP: ", SIGLWP);
+      break;
+    //SIGPOLL is equal to SIGIO on Ubuntu 14.04.
+    case SIGPOLL:
+      printf("%*s %i\n", 25, "SIGPOLL: ", SIGPOLL);
+      break;
+    case SIGTHAW:
+      printf("%*s %i\n", 25, "SIGTHAW: ", SIGTHAW);
+      break;
+    case SIGTHR:
+      printf("%*s %i\n", 25, "SIGTHR: ", SIGTHR);
+      break;
+    case SIGWAITING:
+      printf("%*s %i\n", 25, "SIGWAITING: ", SIGWAITING);
+      break;
+    case SIGXRES:
+      printf("%*s %i\n", 25, "SIGXRES: ", SIGXRES);
+      break;
+  */
     default:
       printf("%*s %i\n", 25, "Undefined Signal: ", signum);
       break;
